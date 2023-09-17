18500516ccf605b0f104daea701613c2b1ae436f
Matteo_psnt <matteopesenti229@gmail.com>
2023-09-16 21:32:41 -0400

    fixed bug with comments

--- expresion_constructor.py
+++ expresion_constructor.py
@@ -6,7 +6,6 @@ def construct_ast(tokens):
     """
     if not tokens:
         return None
-    
     if tokens[0] == '(':
         # Find the matching closing parenthesis for this opening parenthesis
         counter = 1
@@ -17,6 +16,8 @@ def construct_ast(tokens):
             elif tokens[right_paren_index] == ')':
                 counter -= 1
             right_paren_index += 1
+            
+        
 
         # Recurse into the contents of the parenthesis
         inner_expression = construct_ast(tokens[1:right_paren_index - 1])
@@ -27,18 +28,24 @@ def construct_ast(tokens):
             return construct_ast([inner_expression] + rest_expression)
         else:
             return inner_expression
-
     # Process unary operators first
     if tokens[0] == '!':
         return NOT(construct_ast(tokens[1:]))
 
     # Process binary operators
-    for op, cls in [('<=>', IFF), ('=>', IMPLIES), ('&', AND), ('|', OR)]:
-        if op in tokens:
-            i = tokens.index(op)
-            left = construct_ast(tokens[:i])
-            right = construct_ast(tokens[i + 1:])
-            return cls(left, right)
+    comma_hierarchy = 0
+    for op, cls in [('&', AND), ('|', OR), ('=>', IMPLIES), ('<=>', IFF)]:
+        for token in tokens:
+            if token == '(':
+                comma_hierarchy += 1
+            elif token == ')':
+                comma_hierarchy -= 1
+            elif token == op and comma_hierarchy == 0:
+                i = tokens.index(op)
+                left = construct_ast(tokens[:i])
+                right = construct_ast(tokens[i + 1:])
+                return cls(left, right)
+
 
     # Check if it's a binary operator class
     if isinstance(tokens[0], (NOT, AND, OR, IMPLIES, IFF)):
