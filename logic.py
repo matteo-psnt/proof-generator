f9eceb65bb9a701bc09dfd6c0142dc0d7f90e87f
Matteo_psnt <matteopesenti229@gmail.com>
2023-09-21 10:37:28 -0400

    reordered for readability

--- logic.py
+++ logic.py
@@ -1,16 +1,37 @@
+class VAR:
+    def __init__(self, name):
+        self.name = name
 
+    def __str__(self):
+        return str(self.name)
+    
+    def __repr__(self):
+        return "VAR({})".format(str(self.name))
+    
+    def evaluate(self, assignment):
+        return assignment[self.name]
+    
+    def __eq__(self, other):
+        return isinstance(other, VAR) and self.name == other.name
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __len__(self):
+        return 1
+    
 class NOT:
     def __init__(self, expression):
         self.expression = expression
-
-    def evaluate(self, assignment):
-        return not self.expression.evaluate(assignment)
 
     def __str__(self):
         return "(!{})".format(str(self.expression))
     
     def __repr__(self):
         return "NOT({})".format(repr(self.expression))
+    
+    def evaluate(self, assignment):
+        return not self.expression.evaluate(assignment)
     
     def __eq__(self, other):
         return isinstance(other, NOT) and self.expression == other.expression
@@ -30,14 +51,14 @@ class AND:
         self.left = left
         self.right = right
 
-    def evaluate(self, assignment):
-        return self.left.evaluate(assignment) and self.right.evaluate(assignment)
-
     def __str__(self):
         return "({} & {})".format(str(self.left), str(self.right))
     
     def __repr__(self):
         return "AND({}, {})".format(repr(self.left), repr(self.right))
+    
+    def evaluate(self, assignment):
+        return self.left.evaluate(assignment) and self.right.evaluate(assignment)
     
     def __eq__(self, other):
         return isinstance(other, AND) and self.left == other.left and self.right == other.right
@@ -61,14 +82,14 @@ class OR:
         self.left = left
         self.right = right
 
-    def evaluate(self, assignment):
-        return self.left.evaluate(assignment) or self.right.evaluate(assignment)
-
     def __str__(self):
         return "({} | {})".format(str(self.left), str(self.right))
     
     def __repr__(self):
         return "OR({}, {})".format(repr(self.left), repr(self.right))
+    
+    def evaluate(self, assignment):
+        return self.left.evaluate(assignment) and self.right.evaluate(assignment)
 
     def __eq__(self, other):
         return isinstance(other, OR) and self.left == other.left and self.right == other.right
@@ -91,14 +112,14 @@ class IMP:
         self.left = left
         self.right = right
 
-    def evaluate(self, assignment):
-        return (not self.left.evaluate(assignment)) or self.right.evaluate(assignment)
-
     def __str__(self):
         return "({} => {})".format(str(self.left), str(self.right))
     
     def __repr__(self):
         return "IMP({}, {})".format(repr(self.left), repr(self.right))
+    
+    def evaluate(self, assignment):
+        return self.left.evaluate(assignment) and self.right.evaluate(assignment)
     
     def __eq__(self, other):
         return isinstance(other, IMP) and self.left == other.left and self.right == other.right
@@ -121,14 +142,14 @@ class IFF:
         self.left = left
         self.right = right
 
-    def evaluate(self, assignment):
-        return self.left.evaluate(assignment) == self.right.evaluate(assignment)
-
     def __str__(self):
         return "({} <=> {})".format(str(self.left), str(self.right))
     
     def __repr__(self):
         return "IFF({}, {})".format(repr(self.left), repr(self.right))
+    
+    def evaluate(self, assignment):
+        return self.left.evaluate(assignment) and self.right.evaluate(assignment)
     
     def __eq__(self, other):
         return isinstance(other, IFF) and self.left == other.left and self.right == other.right
@@ -146,25 +167,3 @@ class IFF:
         else:
             return len(self.left) + len(self.right) + 1
 
-
-class VAR:
-    def __init__(self, name):
-        self.name = name
-
-    def evaluate(self, assignment):
-        return assignment[self.name]
-
-    def __str__(self):
-        return str(self.name)
-    
-    def __repr__(self):
-        return "VAR({})".format(str(self.name))
-    
-    def __eq__(self, other):
-        return isinstance(other, VAR) and self.name == other.name
-    
-    def __hash__(self):
-        return hash(self.name)
-    
-    def __len__(self):
-        return 1
