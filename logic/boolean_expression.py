5b35678699566be9900c71aeba0c8bd7252fac1e
Matteo_psnt <matteopesenti229@gmail.com>
2023-10-04 20:30:42 -0400

    updated how boolean expressions are parsed

--- logic/boolean_expression.py
+++ logic/boolean_expression.py
@@ -1,26 +1,8 @@
-from itertools import product
-from logic.logic import AND, OR, NOT, IMP, IFF, VAR
 from logic.expression_constructor import construct_ast
-from logic.expression_parser import parse_bool_expression
+from logic.expression_parser import parse_bool_expression, add_parentheses
 
-class Expression:
-    def __init__(self, expression):
-        parsed_expression = parse_bool_expression(expression)
-        self.expression = construct_ast(parsed_expression)
-
-    def evaluate(self, assignment):
-        return self.expression.evaluate(assignment)
-
-    def __str__(self):
-        return str(self.expression)
-    
-    def __repr__(self):
-        return repr(self.expression)
-    
-    def __eq__(self, object: object) -> bool:
-        if not isinstance(object, Expression):
-            return False
-        return self.expression == object.expression
-
-    def __getattr__(self, name):
-        return getattr(self.expression, name)
+def get_expr_from_str(expression_string):
+    parsed_expression = parse_bool_expression(expression_string)
+    ordered_parsed_expression = add_parentheses(parsed_expression)
+    bool_expression = construct_ast(ordered_parsed_expression)
+    return bool_expression
